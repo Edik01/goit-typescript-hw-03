72fff1da84a9ef59cd8c030f8ade07fa67dc2a60
Edik Huloian <guloyan.edik@gmail.com>
2023-11-22 21:55:50 -0800

    sumarry

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,46 @@
-const key = new Key();
+class Key {}
 
+class MyHouse {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  openDoor(personKey: Key): void {
+    if (this.key === personKey) {
+      console.log("Door is opened.");
+    } else {
+      console.log("Access denied. Wrong key.");
+    }
+  }
+
+  comeIn(person: Person): void {
+    console.log(`${person.getName()} entered the house.`);
+  }
+}
+
+class Person {
+  private key: Key;
+  private name: string;
+
+  constructor(key: Key, name: string) {
+    this.key = key;
+    this.name = name;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
-const person = new Person(key);
+const person = new Person(key, "John");
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
-
-export {};
